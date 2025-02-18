f5880db6fe7af261da2db681b6d71d7ae2113eea
ruzica-glavas <ruzi99@hotmail.it>
2025-02-18 17:00:10 +0100

    add: all element in Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Header from "./components/Header"
+
+
+
+
 function App() {
-    return 
+    return (
+      <>
+        <Header />
+      </>
+    )
   }
 
 export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header =()=>{
+    return(
+        <header>
+            <figure>
+                <img src="/dc-logo.png" alt="logo"></img>
+            </figure>
+                <nav>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Collectibles</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">Fans</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Shop</a></li>
+                    </ul>
+                </nav> 
+        </header>
+    )
+}
+
+export default Header
