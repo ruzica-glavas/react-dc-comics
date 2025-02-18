df8fee6dee7e76a12fde581ef5be857e6b9d9a50
ruzica-glavas <ruzi99@hotmail.it>
2025-02-18 17:35:21 +0100

    add: Main.jsx in App.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,42 @@
+const Main = () =>{
+    return(
+        <main>
+            <section id="click">
+                <a className="content-click" href="#">Content goes Here</a>
+            </section>
+            
+            <section id="products">
+                <ul>
+                    <li>
+                        <a className="content" href="#">Digital Comics</a>
+                    </li>
+
+                    <li>
+                        <a className="content" href="#">Dc Merchandise</a>
+                    
+                    </li>
+
+                    <li>
+                        
+                        <a className="content" href="#">Subscription</a>
+                    
+                    </li>
+
+                    <li>
+                        
+                        <a className="content" href="#">Comic Shop Locator</a>
+                    </li>
+
+                    <li>
+                        
+                        <a className="content" href="#">Dc Power Visa</a>
+                    </li>
+                </ul>
+            </section>
+        </main>
+        
+       
+    )
+}
+
+export default Main
