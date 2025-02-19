1e52dd99b6a2a945e0eec546064288a590f61f92
ruzica-glavas <ruzi99@hotmail.it>
2025-02-19 16:09:50 +0100

    add: correct the code, now it works

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-const links=[
-    {id: 1, text: Characters, url: '#'},
-    {id: 2, text: Comics, url: '#'},
-    {id: 3, text: Movies, url: '#'},
-    {id: 4, text: TV, url: '#'},
-    {id: 5, text: Games, url: '#'},
-    {id: 6, text: Collectibles, url: '#'},
-    {id: 7, text: Videos, url: '#'},
-    {id: 8, text: Fans, url: '#'},
-    {id: 9, text: News, url: '#'},
-    {id: 10, text: Shop, url: '#'},
-]
 
 
-const Header =(links)=>{
+const Header =()=>{
+
+    const links=[
+        {id: 1, text: "Characters", url: '#'},
+        {id: 2, text: "Comics", url: '#'},
+        {id: 3, text: "Movies", url: '#'},
+        {id: 4, text: "TV", url: '#'},
+        {id: 5, text: "Games", url: '#'},
+        {id: 6, text: "Collectibles", url: '#'},
+        {id: 7, text: "Videos", url: '#'},
+        {id: 8, text: "Fans", url: '#'},
+        {id: 9, text: "News", url: '#'},
+        {id: 10, text: "Shop", url: '#'},
+    ]
+    
     return(
         <header>
             <figure>
@@ -20,16 +22,26 @@ const Header =(links)=>{
             </figure>
                 <nav>
                     <ul className="list-horrizzontal">
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
-                        <li><a href="#">{ links.text }</a></li>
+
+                        {
+                            links.map((element) =>
+                                <li key = {element.id}>
+                                    <a href ={element.url}>{element.text}</a>
+                                </li>
+                        )
+                        }
+
+
+                        {/*<li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Collectibles</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">Fans</a></li>
+                        <li><a href="#">News</a></li>
+                        <li><a href="#">Shop</a></li>*/}
                     </ul>
                 </nav> 
         </header>
