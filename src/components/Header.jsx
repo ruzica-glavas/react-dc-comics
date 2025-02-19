9238e8f62b805714b5a06780b332a4ae10ff30d0
ruzica-glavas <ruzi99@hotmail.it>
2025-02-19 15:41:37 +0100

    add: create array in header and put the dinamic part into the code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,18 @@
-const Header =()=>{
+const links=[
+    {id: 1, text: Characters, url: '#'},
+    {id: 2, text: Comics, url: '#'},
+    {id: 3, text: Movies, url: '#'},
+    {id: 4, text: TV, url: '#'},
+    {id: 5, text: Games, url: '#'},
+    {id: 6, text: Collectibles, url: '#'},
+    {id: 7, text: Videos, url: '#'},
+    {id: 8, text: Fans, url: '#'},
+    {id: 9, text: News, url: '#'},
+    {id: 10, text: Shop, url: '#'},
+]
+
+
+const Header =(links)=>{
     return(
         <header>
             <figure>
@@ -6,16 +20,16 @@ const Header =()=>{
             </figure>
                 <nav>
                     <ul className="list-horrizzontal">
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Collectibles</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">Fans</a></li>
-                        <li><a href="#">News</a></li>
-                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
+                        <li><a href="#">{ links.text }</a></li>
                     </ul>
                 </nav> 
         </header>
