003f73500ae9ef644bfd8b7dca5150f9acd0a491
ruzica-glavas <ruzi99@hotmail.it>
2025-02-20 16:50:48 +0100

    add: corret class in className in Main and change an image in ComicsList bacause the image with id:5 doesn't exist

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,29 +13,29 @@ const Main = () =>{
             <section id="products">
                 <ul className="list-horrizzontal">
                     <li className="list-item-main">
-                        <img class= "img-main" src="src\assets\img\buy-comics-digital-comics.png"></img>
+                        <img className= "img-main" src="src\assets\img\buy-comics-digital-comics.png"></img>
                         <a className="content" href="#">Digital Comics</a>
                     </li>
 
                     <li className="list-item-main">
-                        <img class= "img-main" src="src\assets\img\buy-comics-merchandise.png"></img>
+                        <img className= "img-main" src="src\assets\img\buy-comics-merchandise.png"></img>
                         <a className="content" href="#">Dc Merchandise</a>
                     
                     </li>
 
                     <li className="list-item-main">
-                        <img class= "img-main" src="src\assets\img\buy-comics-subscriptions.png"></img>
+                        <img className= "img-main" src="src\assets\img\buy-comics-subscriptions.png"></img>
                         <a className="content" href="#">Subscription</a>
                     
                     </li>
 
                     <li className="list-item-main">
-                        <img class= "img-main" src="src\assets\img\buy-comics-shop-locator.png"></img>
+                        <img className= "img-main" src="src\assets\img\buy-comics-shop-locator.png"></img>
                         <a className="content" href="#">Comic Shop Locator</a>
                     </li>
 
                     <li className="list-item-main">
-                        <img class= "img-main" src="src\assets\img\buy-dc-power-visa.svg"></img>
+                        <img className= "img-main" src="src\assets\img\buy-dc-power-visa.svg"></img>
                         <a className="content" href="#">Dc Power Visa</a>
                     </li>
                 </ul>
